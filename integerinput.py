d4af6575ebf47c56bb94507a9cf668a992343d03
F1D022035_Apta Mahogra Bhamakerti <88429357+NMahogra@users.noreply.github.com>
2025-05-06 22:00:52 +0800

    Add files via upload

---
+++ integerinput.py
@@ -0,0 +1,23 @@
+# integerinput.py
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setWindowTitle("Integer Input")
+        MainWindow.resize(400, 200)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+
+        self.label = QtWidgets.QLabel("Enter an integer:", self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(40, 40, 120, 20))
+
+        self.spinBox = QtWidgets.QSpinBox(self.centralwidget)
+        self.spinBox.setGeometry(QtCore.QRect(160, 40, 100, 22))
+
+        self.pushButton = QtWidgets.QPushButton("OK", self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(80, 100, 60, 30))
+
+        self.pushButton_2 = QtWidgets.QPushButton("Cancel", self.centralwidget)
+        self.pushButton_2.setGeometry(QtCore.QRect(160, 100, 60, 30))
+
+        MainWindow.setCentralWidget(self.centralwidget)
